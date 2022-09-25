e6481cfee0bb817b380540612bc1788b725b0fa8
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-09-25 13:40:41 +0300

    DONE

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,7 +8,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(birds_param)
     render json: bird, status: :created
   end
 
@@ -20,6 +20,12 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+
+  private 
+  def birds_param
+    params.permit(:name, :species)
   end
 
 end
